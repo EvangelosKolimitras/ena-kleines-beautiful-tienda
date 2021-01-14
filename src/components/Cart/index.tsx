c1a91a61863e6610b2032abb384fc69325f6e06b
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-15 00:08:37 +0100

    feat: Adds conditional rendering
    When the cart has no items in it a different view will be displayed.

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Container, Grid, Typography } from '@material-ui/core'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { CartItem } from '../CartItem'
 import { IProduct } from '../Products'
 import { useStyles } from './styles'
@@ -25,13 +26,15 @@ export const Cart: React.FC<CartProps> = (props): JSX.Element => {
 		<>
 			<Grid container spacing={3}>
 				{
-					cart.line_items.map((item: IProduct) => {
-						return (
-							<Grid item xs={12} sm={4} key={cart.id}>
-								<CartItem item={item} />
-							</Grid>
-						)
-					})
+					cart.line_items ?
+						cart.line_items.map((item: IProduct) => {
+							return (
+								<Grid item xs={12} sm={4} key={cart.id}>
+									<CartItem item={item} />
+								</Grid>
+							)
+						}) :
+						<Typography variant="h6" color="primary">No items in the basket. <Link className={classes.link} to="/">Add some</Link>!</Typography>
 				}
 			</Grid>
 			<Box component="div" className={classes.cardDetails}>
