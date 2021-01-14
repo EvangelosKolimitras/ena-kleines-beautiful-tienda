17c1d9cd7955ba3f8b077fc8e38a58a1bf45b953
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-15 00:00:13 +0100

    feat: Adds links for navigating to / and  /cart

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Badge, Box, IconButton, Toolbar, Typography } from '@material-ui/core'
 import { ShoppingBasket } from '@material-ui/icons'
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/shops.png'
 import { useStyles } from "./styles"
 
@@ -10,23 +11,29 @@ interface NavBarProps {
 
 export const NavBar: React.FC<NavBarProps> = (props): JSX.Element => {
 	const { totatItemsInBasket } = props
+	const location = useLocation().pathname;
 	const classes = useStyles()
+
 	return (
 		<>
 			<AppBar position="fixed" className={classes.appBar} color="inherit">
 				<Toolbar>
-					<Typography variant="h6" color="inherit" className={classes.title}>
+					<Typography component={Link} to="/" variant="h6" color="inherit" className={classes.title}>
 						<img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
 						EKBT
 					</Typography>
 					<Box component="div" className="classes.grow" />
-					<Box component="div" className={classes.menuButton}>
-						<IconButton aria-label="Show cart items" color="inherit">
-							<Badge badgeContent={totatItemsInBasket} color="secondary">
-								<ShoppingBasket />
-							</Badge>
-						</IconButton>
-					</Box>
+					{
+						// If we are on the /cart route then we do not render the cart icon
+						location === "/" &&
+						<Box component="div" className={classes.menuButton}>
+							<IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+								<Badge badgeContent={totatItemsInBasket} color="secondary">
+									<ShoppingBasket />
+								</Badge>
+							</IconButton>
+						</Box>
+					}
 				</Toolbar>
 			</AppBar>
 		</>
